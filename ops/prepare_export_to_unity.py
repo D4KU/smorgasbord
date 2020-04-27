fa6df4273166f0d812a0c3d1ed94593ebc63a96c
D4KU <david.kutschke@arcor.de>
2020-04-27 23:22:49 +0200

    prepare_export_to_unity.py: bone roll gets zeroed now

--- ops/prepare_export_to_unity.py
+++ ops/prepare_export_to_unity.py
@@ -6,16 +6,21 @@ from smorgasbord.common.decorate import register
 class PrepareExportToUnity(bpy.types.Operator):
     bl_idname = "transform.prepare_export_to_unity"
     bl_label = "Prepare Export to Unity"
-    bl_description = "Set rotation and scale to match object transform in"
-    "Unity game engine. Destructive. Fails on linked data."
+    bl_description = (
+        "Set rotation and scale to match object transform in "
+        "the Unity game engine. Destructive. Fails on linked data."
+        )
     bl_options = {'REGISTER', 'UNDO'}
     menus = [
         bpy.types.VIEW3D_MT_transform_object,
     ]
 
-    prepare_rotation: bpy.props.BoolProperty(
-        name="Prepare Rotation",
-        description="Convert Z-Up to Y-Up",
+    prepare_orientation: bpy.props.BoolProperty(
+        name="Prepare Orientation",
+        description=(
+            "Convert right-handed to left-handed coordinate "
+            "system"
+            ),
         default=True,
     )
 
@@ -25,8 +30,12 @@ class PrepareExportToUnity(bpy.types.Operator):
         default=True,
     )
 
+    @classmethod
+    def poll(cls, context):
+        return context.mode == 'OBJECT'
+
     def execute(self, context):
-        if self.prepare_rotation:
+        if self.prepare_orientation:
             bpy.ops.transform.resize(value=(-1, -1, 1))
             bpy.ops.transform.rotate(value=1.5708, orient_axis='X')
             bpy.ops.object.transform_apply(
@@ -35,6 +44,17 @@ class PrepareExportToUnity(bpy.types.Operator):
                 scale=False,
                 )
             bpy.ops.transform.rotate(value=-1.5708, orient_axis='X')
+
+            # zero all bone rolls
+            for o in context.selected_objects:
+                if o.type != 'ARMATURE':
+                    continue
+
+                bpy.ops.object.mode_set(
+                    {'active_object': o}, mode='EDIT')
+                for b in o.data.edit_bones:
+                    b.roll = 0
+                bpy.ops.object.mode_set(mode='OBJECT')
 
         if self.prepare_scale:
             bpy.ops.transform.resize(value=(100, 100, 100))
