4f162d74bc9a9298643dcaba911468f7c0145eb4
D4KU <david.kutschke@arcor.de>
2020-06-01 18:23:58 +0200

    close_through_holes.py can now handle loops inside the hole

--- ops/close_through_holes.py
+++ ops/close_through_holes.py
@@ -2,7 +2,11 @@ import bmesh as bm
 import bpy
 import numpy as np
 
-from smorgasbord.common.io import get_vecs, get_bounds_and_center, get_scalars
+from smorgasbord.common.io import (
+    get_bounds_and_center,
+    get_scalars,
+    get_vecs,
+)
 from smorgasbord.common.decorate import register
 
 
@@ -13,7 +17,7 @@ class CloseSolidHoles(bpy.types.Operator):
     bl_description = (
         "Close through-going or blind holes in the selected geometry. "
         "These are aren't holes caused by missing faces, but valid "
-        "geometry. Think of holes caused by a drilling machine."
+        "geometry. Think of holes in a cheese."
     )
     bl_options = {'REGISTER', 'UNDO'}
     menus = [bpy.types.VIEW3D_MT_transform]
@@ -29,11 +33,13 @@ class CloseSolidHoles(bpy.types.Operator):
     _limits = (0, 1)
     limits: bpy.props.FloatVectorProperty(
         name="Size Limits",
-        description=(""),
+        description=(
+            "Holes whose diameter lies between (min, max] get closed"
+        ),
         size=2,
         step=10,
         default=_limits,
-        min=0.0,
+        min=0,
         get=_get_limits,
         set=_set_limits,
     )
@@ -48,6 +54,13 @@ class CloseSolidHoles(bpy.types.Operator):
         return self.execute(context)
 
     def _find_concave_patches(self, context):
+        """
+        A patch is a set of connected faces. For each patch containing
+        at least two faces facing each other, return a list of vertex
+        indices making up such a patch, together with its center point
+        and diameter. A patch's border is along edges between faces
+        facing away from each other (think ridges).
+        """
         self._meshes.clear()
         for o in context.objects_in_mode_unique_data:
             o.update_from_editmode()
@@ -65,7 +78,6 @@ class CloseSolidHoles(bpy.types.Operator):
             # Will contain a list of tuples. First entry is the list of
             # face indices of the patch. Second is the dimension it's
             # facing.
-            # A patch is a set of connected faces.
             # This list is only needed to not delete vertices while we
             # iterate the mesh.
             patches = []
@@ -79,6 +91,7 @@ class CloseSolidHoles(bpy.types.Operator):
                 stack = [bfaces[i]]
                 # Face indices of the patch
                 findcs = []
+                concave = False
 
                 while stack:
                     f = stack.pop()
@@ -90,13 +103,23 @@ class CloseSolidHoles(bpy.types.Operator):
                     for l in f.loops:
                         f2 = l.link_loop_radial_next.face
                         i2 = f2.index
-                        # but only if not already checked and
-                        # f and f2 are concave to each other
-                        if flags[i2] and n.dot(centrs[i2] - c) > 1e-5:
+                        # The dot product between f's normal and the
+                        # vector between both face's centers is a
+                        # simple way to measure if they are parallel
+                        # (=0), concave (>0), or convex (<0).
+                        angl = n.dot(centrs[i2] - c)
+                        # but only if not already checked and f and f2
+                        # are not convex (don't face away from each 
+                        # other), but...
+                        if flags[i2] and angl > -1e-3:
+                            # at least two faces have to face each
+                            # other, allowing for some error margin.
+                            if angl > 1e-4:
+                                concave = True
                             flags[i2] = False
                             stack.append(f2)
 
-                if len(findcs) > 1:
+                if len(findcs) > 2 and concave:
                     patches.append(findcs)
 
             del flags
@@ -125,20 +148,28 @@ class CloseSolidHoles(bpy.types.Operator):
 
     def execute(self, context):
         mind, maxd = self._limits
+        # Vertex indices already deleted in earlier patches that we
+        # can't delete again.
+        dvindcs = np.empty(0, dtype=int)
+
+        # Iterate over results computed during invoke()
         for mesh, patches in self._meshes:
             bob = bm.from_edit_mesh(mesh)
             bverts = np.array(bob.verts)
             for vindcs, centr, diam in patches:
                 # Only close holes whose diameter lies within limits
-                if not mind < diam < maxd:
+                if not mind < diam <= maxd:
                     continue
 
+                # Remove all vertices that already got deleted
+                vindcs = np.setdiff1d(vindcs, dvindcs)
                 # Merge all vertices of patch to first vertex in list
                 vs = bverts[vindcs]
                 bm.ops.pointmerge(bob, verts=vs, merge_co=centr)
                 # Dissolve last remaining vertex of patch
                 bm.ops.dissolve_verts(bob, verts=[vs[0]])
-
+                del vs
+                dvindcs = np.concatenate((dvindcs, vindcs))
             bm.update_edit_mesh(mesh)
         return {'FINISHED'}
 
