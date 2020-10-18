28b4a3d6945ae6e59dce7cfcbdc56e4a2407b872
D4KU <d4k@tuta.io>
2020-10-18 16:49:05 +0200

    Reworked prepare_export_to_unity.py entirely. Code not cleaned up or documented yet, but already working in most cases.

--- ops/prepare_export_to_unity.py
+++ ops/prepare_export_to_unity.py
@@ -1,9 +1,40 @@
 import bpy
+from mathutils import Matrix
+import numpy as np
 from math import pi
 from smorgasbord.common.decorate import register
+from smorgasbord.common.io import get_vecs, set_vals
+from smorgasbord.common.transf import transf_vecs
 
 
 pihalf = pi * 0.5
+
+# Transforms the object's data so that the object does not end up
+# mirrored in Unity.
+mat = Matrix(((0, -1, 0, 0),
+              (0,  0, 1, 0),
+              (1,  0, 0, 0),
+              (0,  0, 0, 1)))
+
+# Rotates the object so that after 'posmat' sets its location, it ends
+# up with a (90, 0, 0) rotation vector.
+met = Matrix(((-1, 0,  0, 0),
+              (0,  0,  1, 0),
+              (0,  1,  0, 0),
+              (0,  0,  0, 1)))
+
+# posmat @ met
+# posmet = Matrix(((1, 0,  0, 0),
+#               (0, 0, -1, 0),
+#               (0, 1,  0, 0),
+#               (0, 0,  0, 1)))
+
+# Places the object so that in Unity it ends up in the same place as in
+# Blender. It a rotation of 180 degrees around z.
+posmat = Matrix(((-1,  0, 0, 0),
+                 ( 0, -1, 0, 0),
+                 ( 0,  0, 1, 0),
+                 ( 0,  0, 0, 1)))
 
 
 @register
@@ -17,38 +48,10 @@ class PrepareExportToUnity(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     menus = [bpy.types.VIEW3D_MT_transform_object]
 
-    prepare_orientation: bpy.props.BoolProperty(
-        name="Prepare Orientation",
+    invert: bpy.props.BoolProperty(
+        name="Invert",
         description=\
-            "Rotate selection 90 degrees around x-axis",
-        default=True,
-    )
-    inv_rot: bpy.props.BoolProperty(
-        name="Inverse Rotation",
-        description=\
-            "Rotate selection -90 degrees instead. Only takes effect "
-            "if 'Prepare Orientation' is true",
-        default=False,
-    )
-    correct_bone_roll: bpy.props.BoolProperty(
-        name="Correct Bone Roll",
-        description=\
-            "Iterate over every selected armature and subtract 90 "
-            "degrees from every bone's roll angle. Only takes effect "
-            "if 'Prepare Orientation' is true",
-        default=False,
-    )
-    swap_left_right: bpy.props.BoolProperty(
-        name="Swap Left Right",
-        description=\
-            "Swap left and right to fully convert the right-handed to "
-            "a left-handed coordinate system. Only takes effect if "
-            "'Prepare Orientation' is true",
-        default=False,
-    )
-    prepare_scale: bpy.props.BoolProperty(
-        name="Prepare Scale",
-        description="Convert object scale from cm to m",
+            "",
         default=False,
     )
 
@@ -57,44 +60,45 @@ class PrepareExportToUnity(bpy.types.Operator):
         return context.mode == 'OBJECT'
 
     def execute(self, context):
-        if self.prepare_orientation:
-            if self.swap_left_right:
-                bpy.ops.transform.resize(
-                    value=(-1, -1, 1),
-                    center_override=(0, 0, 0),
-                )
+        if self.invert:
+            mad = mat.inverted()
+            med = met.inverted()
+            posmad = posmat.inverted()
+        else:
+            mad = mat
+            med = met
+            posmad = posmat
+        datas = {None}
+        arms = []
+        obs = sorted(context.selected_editable_objects, key=get_lvl)
+        for o in obs:
+            data = o.data
+            if data not in datas:
+                data.transform(mad)
+                datas.add(data)
+                if o.type == 'MESH':
+                    data.flip_normals()
+                if o.type == 'ARMATURE':
+                    arms.append(o)
+            o.matrix_world = posmad @ o.matrix_world @ med
 
-            angl = -pihalf if self.inv_rot else pihalf
-            bpy.ops.transform.rotate(value=angl, orient_axis='X')
-            bpy.ops.object.transform_apply(
-                location=False,
-                rotation=True,
-                scale=self.swap_left_right,
-                )
-            bpy.ops.transform.rotate(value=-angl, orient_axis='X')
+        for o in arms:
+            bpy.ops.object.mode_set(
+                {'active_object': o}, mode='EDIT')
+            for b in o.data.edit_bones:
+                b.roll -= pihalf
+            bpy.ops.object.mode_set(mode='OBJECT')
 
-            if self.correct_bone_roll:
-                for o in context.selected_objects:
-                    if o.type != 'ARMATURE':
-                        continue
-
-                    bpy.ops.object.mode_set(
-                        {'active_object': o}, mode='EDIT')
-                    for b in o.data.edit_bones:
-                        # TODO test if 2*angl is actually necessary
-                        b.roll -= angl
-                    bpy.ops.object.mode_set(mode='OBJECT')
-
-        if self.prepare_scale:
-            bpy.ops.transform.resize(value=(100, 100, 100))
-            bpy.ops.object.transform_apply(
-                location=False,
-                rotation=False,
-                scale=True,
-                )
-            bpy.ops.transform.resize(value=(0.01, 0.01, 0.01))
-
+        # context.view_layer.update()
         return {'FINISHED'}
+
+
+def get_lvl(ob):
+    lvl = -1
+    while ob:
+        ob = ob.parent
+        lvl += 1
+    return lvl
 
 
 if __name__ == "__main__":
