2c1a5f0b1b21d840653a1377fc14e49c0f979043
D4KU <d4k@tuta.io>
2021-03-17 19:16:35 +0100

    Fixed bug and added regex to Select By Name

--- ops/select_by_name.py
+++ ops/select_by_name.py
@@ -1,16 +1,15 @@
 import bpy
+import re
 from smorgasbord.common.decorate import register
 
 
 @register
-class SelectAllByName(bpy.types.Operator):
-    bl_idname = "object.select_all_by_name"
-    bl_label = "Select All by Name"
-    bl_description = "Select all objects in scene with a given name"
+class SelectByName(bpy.types.Operator):
+    bl_idname = "object.select_by_name"
+    bl_label = "Select by Name"
+    bl_description = "Select all objects in view layer with a given name"
     bl_options = {'REGISTER', 'UNDO'}
-    menus = [
-        bpy.types.VIEW3D_MT_select_object
-    ]
+    menus = [bpy.types.VIEW3D_MT_select_object]
 
     name: bpy.props.StringProperty(
         name = "Name",
@@ -24,9 +23,9 @@ class SelectAllByName(bpy.types.Operator):
             ('EQUALS', "Equals", "Object name must equal string"),
             ('STARTS_WITH', "Starts with", "Object name must start with string"),
             ('ENDS_WITH', "Ends with", "Object name must end with string"),
+            ('REGEX', "Regex", "Interpret name as regular expression"),
         ]
     )
-
 
     def execute(self, context):
         if self.match_metric == 'CONTAINS':
@@ -35,15 +34,13 @@ class SelectAllByName(bpy.types.Operator):
             compare = lambda a, b: a == b
         elif self.match_metric == 'STARTS_WITH':
             compare = lambda a, b: a.startswith(b)
-        else:
+        elif self.match_metric == 'ENDS_WITH':
             compare = lambda a, b: a.endswith(b)
+        else:
+            compare = lambda a, b: re.fullmatch(b, a)
 
-        for o in context.scene.objects:
+        for o in context.view_layer.objects:
             if compare(o.name, self.name):
                 o.select_set(True)
 
         return {'FINISHED'}
-
-
-if __name__ == "__main__":
-    register()
