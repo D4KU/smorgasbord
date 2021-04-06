d9f5140cf569693db77f2a9713c4a7d310d8b1fd
D4KU <d4k@tuta.io>
2021-04-06 21:32:37 +0200

    An error on import now only stops the broken operator from registering, not the whole plugin

--- ops/__init__.py
+++ ops/__init__.py
@@ -1,31 +1,36 @@
-from smorgasbord.ops import (
-    align_bounds,
-    apply_name,
-    force_apply_modifier,
-    force_apply_transform,
-    force_join,
-    join_as_vertex_group,
-    lerp_weight,
-    material_transfer,
-    offset_selection,
-    prepare_export_to_unity,
-    prepend_name_to_children,
-    quick_fix_objects,
-    remove_similar_uv_maps,
-    remove_empty_vertex_groups,
-    rename_vertex_groups,
-    replace_by_primitive,
-    replace_duplicate_materials,
-    select_by_name,
-    select_concave_parts,
-    select_visible,
-    select_loose_by_size,
-    select_n_instances,
-    select_similar,
-    set_parent_advanced,
-    smart_parent_to_empty,
-    vertex_color_to_group,
-)
+from importlib import import_module
+for m in [
+    ".align_bounds",
+    ".apply_name",
+    ".force_apply_modifier",
+    ".force_apply_transform",
+    ".force_join",
+    ".join_as_vertex_group",
+    ".lerp_weight",
+    ".material_transfer",
+    ".offset_selection",
+    ".prepare_export_to_unity",
+    ".prepend_name_to_children",
+    ".quick_fix_objects",
+    ".remove_similar_uv_maps",
+    ".remove_empty_vertex_groups",
+    ".rename_vertex_groups",
+    ".replace_by_primitive",
+    ".replace_duplicate_materials",
+    ".select_by_name",
+    ".select_concave_parts",
+    ".select_visible",
+    ".select_loose_by_size",
+    ".select_n_instances",
+    ".select_similar",
+    ".set_parent_advanced",
+    ".smart_parent_to_empty",
+    ".vertex_color_to_group",
+]:
+    try:
+        import_module(m, package=__name__)
+    except Exception as e:
+        print("Couldn't register " + __name__ + m + ": " + str(e))
 
 
 def _call_globals(attr_name):
@@ -40,7 +45,3 @@ def register():
 
 def unregister():
     _call_globals("unregister")
-
-
-if __name__ == '__main__':
-    register()
