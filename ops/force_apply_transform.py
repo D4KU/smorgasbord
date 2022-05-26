414924d422658123d366174d77024bae3f741bcd
D4KU <d4k@tuta.io>
2022-05-26 13:28:48 +0200

    Redesigned force_apply_transform to make better use of multi-selection

--- ops/force_apply_transform.py
+++ ops/force_apply_transform.py
@@ -2,6 +2,7 @@ import bpy
 from mathutils import Matrix
 
 from smorgasbord.common.decorate import register
+from smorgasbord.common.hierarchy import get_lvl
 
 
 @register
@@ -9,12 +10,10 @@ class ForceApplyTransform(bpy.types.Operator):
     bl_idname = "object.force_apply_transform"
     bl_label = "Force Apply Transform"
     bl_description = (
-        "Apply the active object's transform to its mesh. If the data "
-        "is shared, the transformation of all instances is adjusted so "
-        "that they don't move. If other instances are selected besides "
-        "the active object, the adjustment is only applied to those"
+        "Apply every selected object's local transform to its mesh. If "
+        "the data is shared, the transformation of all instances is "
+        "adjusted so that they don't move"
     )
-
     bl_options = {'REGISTER', 'UNDO'}
     menus = [bpy.types.VIEW3D_MT_object_apply]
 
@@ -29,27 +28,18 @@ class ForceApplyTransform(bpy.types.Operator):
         ),
         default='ALL',
     )
-    round_to: bpy.props.IntProperty(
-        name="Round to digits",
-        description="Round transform entries to this number of digits",
-        default=6,
-    )
     to_cursor: bpy.props.BoolProperty(
         name="To Cursor",
-        description="Regard cursor as origin",
+        description="Treat cursor as origin",
         default=False,
     )
 
     @classmethod
     def poll(cls, context):
-        a = context.mode == 'OBJECT'
-        b = bool(context.object)
-        return a and b and context.object.type == 'MESH'
+        return context.mode == 'OBJECT'
 
-    def execute(self, context):
-        ob = context.object
-
-        # Build matrix to transform mesh data
+    # Build matrix to transform mesh data dependent on user choices
+    def _get_tmat(self, context, ob):
         if self.apply_to == 'LOC':
             tmat = Matrix.Translation(ob.matrix_local.translation)
         elif self.apply_to == 'ROT':
@@ -57,57 +47,54 @@ class ForceApplyTransform(bpy.types.Operator):
         elif self.apply_to == 'SCL':
             tmat = Matrix.Diagonal(ob.matrix_local.to_scale()).to_4x4()
         else:
-            tmat = ob.matrix_local
+            tmat = ob.matrix_local.copy()
 
         if self.to_cursor:
-            tmat = context.scene.cursor.matrix.inverted() @ tmat
+            return context.scene.cursor.matrix.inverted() @ tmat
+        return tmat
 
-        ob.data.transform(tmat)
-        tmat_inv = tmat.inverted()
+    def execute(self, context):
+        # meshes, armatures, ... transformed by this operator
+        datas = dict()
 
-        # If no additional objects are selected in addition to the
-        # active one, consider all objects in the scene
-        selobs = context.selected_editable_objects
-        if selobs == [ob] or not selobs:
-            selobs = context.scene.objects
+        # Transform data of every selected object and cache the
+        # applied transformation
+        for o in context.selected_editable_objects:
+            if hasattr(o.data, 'transform') and o.data not in datas:
+                tmat = self._get_tmat(context, o)
+                datas[o.data] = tmat
+                o.data.transform(tmat)
 
-        # Update instances (objects sharing the same mesh as 'ob'),
-        # including 'ob' itself
-        for o in selobs:
-            if o.data is not ob.data:
-                continue
+        # Traverse scene graph top-down. Even unselected objects must be
+        # transformed if sharing data with a selected one.
+        for o in sorted(context.scene.objects, key=get_lvl):
+            # For every object, find the transformation applied to its
+            # data.
+            tmat = datas.get(o.data, None)
+            if not tmat:
+                # Even if o's data hasn't been transformed, get its
+                # transform if selected.
+                if o.select_get():
+                    tmat = self._get_tmat(context, o)
+                else:
+                    continue
 
-            # Update children of instances to let them visually stay in
-            # place
+            # Move the object opposite to its data
+            o.matrix_local = o.matrix_local @ tmat.inverted()
+
+            # Update children to let them visually stay in place
             for child in o.children:
-                self._set_mat_local(child, tmat @ child.matrix_local)
+                child.matrix_local = tmat @ child.matrix_local
 
-            # matrix_local is updated only after execution of this
-            # script, so objects still return their transformation from
-            # before the operator, even if it has been set before
-            if o.parent and o.parent.data is ob.data:
-                # 'o' is an instance AND child to an instance
-                self._set_mat_local(o, tmat @ o.matrix_local @ tmat_inv)
-            else:
-                self._set_mat_local(o, o.matrix_local @ tmat_inv)
+            # Without this update, matrix_local is only updated after
+            # this script runs, so objects still return their
+            # transformation from before the operator, even if it has
+            # been set before.
+            context.view_layer.update()
 
         # What does o.matrix_local turn into?
         # * no child, no instance: o.matrix_local
         # * child, no instance: tmat @ o.matrix_local
-        # * no child, instance: o.matrix_local @ tmat_inv
-        # * child, instance: tmat @ o.matrix_local @ tmat_inv
+        # * no child, instance: o.matrix_local @ tmat.inverted()
+        # * child, instance: tmat @ o.matrix_local @ tmat.inverted()
         return {'FINISHED'}
-
-    def _set_mat_local(self, ob, mat):
-        round_mat(mat, self.round_to)
-        ob.matrix_local = mat
-
-
-def round_mat(mat, ndigits=6):
-    """
-    Round every entry in the given matrix to consist of maximally
-    n digits
-    """
-    for row in mat:
-        for i in range(len(row)):
-            row[i] = round(row[i], ndigits)
